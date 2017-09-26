d605d3ed3ecc64827e4125c5f64f37946dbf9027
TeoResende <resendenetto@yahoo.com.br>
2017-09-26 14:37:59 -0300

    Initial commit

---
+++ app/src/main/java/br/com/senai/clientewebservice2017/MainActivity.java
@@ -0,0 +1,18 @@
+package br.com.senai.clientewebservice2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
